374b19f93d89fe5006caa8ba6281985e432ccc76
Michael Eden <themichaeleden@gmail.com>
2015-02-23 00:11:27 -0500

    Added SQL node packages

--- PeopleInTables/Methods.js
+++ PeopleInTables/Methods.js
@@ -1,5 +1,16 @@
+var pg    = Meteor.npmRequire('pg');
+var squel = Meteor.npmRequire('squeal');
+
+pg.connect(Meteor.settings.postgres.url, function(err, client, done) {
+    if (err) {
+        return console.error('Error getting client from pool', err);
+    }
+
+
+});
+
 Meteor.methods({
-    getPeople: function(data, callback) {
+    getPeople: function(data) {
         console.log(data);
         return {
             "draw": data.draw,
